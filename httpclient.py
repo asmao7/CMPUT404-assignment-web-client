893db5df5bd19c99391253682d345bb0d0218f93
soomyy <aaali@ualberta.ca>
2021-02-12 12:34:24 -0700

    passed tests

--- httpclient.py
+++ httpclient.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # coding: utf-8
 # Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
+# Copyright @student submitting: Asma Omar
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,7 +23,8 @@ import sys
 import socket
 import re
 # you may use urllib to encode data appropriately
-import urllib.parse
+#import urllib.parse
+from urllib.parse import urlparse, urlencode
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
@@ -40,14 +42,19 @@ class HTTPClient(object):
         self.socket.connect((host, port))
         return None
 
+    # get code if it starts with http
     def get_code(self, data):
-        return None
+        code = data.split()[1]
+        return int(code)
+        
 
     def get_headers(self,data):
         return None
 
+    # return body from data
     def get_body(self, data):
-        return None
+        body = data.split("\r\n\r\n")[1]
+        return body
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -67,14 +74,79 @@ class HTTPClient(object):
                 done = not part
         return buffer.decode('utf-8')
 
+    def parse(self, url):
+        parse_result = urlparse(url)
+        print("PARSE RESULTS: ", parse_result)
+        self.host_name = parse_result.hostname #parse_result.netloc
+        print("HOST NAME IS: ", self.host_name)
+        self.host = self.host_name.split(":")[0]
+        print("HOST IS: ", self.host)
+        self.port = parse_result.port
+        print("PORT IS: ", self.port)
+        if(self.port == None):
+            self.port = 80
+        self.path = parse_result.path
+        if(self.path == ""):
+            self.path = "/"
+
+
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        # if not http in url 404 error otherwise connect
+        if('http' not in url and 'http'.upper() not in url and 'https' not in url and 'https'.upper() not in url):
+            code = 404
+            body = 'HTTP/1.1 404 Not Found'
+            return HTTPResponse(code, body)
+        
+        # parse url
+        self.parse(url)
+        request = f"GET {(self.path)} HTTP/1.1\r\nHost: {(self.host_name)} \r\nConnection: close\r\n\r\n"
+        print("REQUEST IS: ", request)
+
+        # connect
+        self.connect(self.host, self.port)
+        self.sendall(request)
+        
+        # receive data
+        data = self.recvall(self.socket)
+        self.close()
+
+        # get code and body then return HTTP response
+        code = self.get_code(data)
+        body = self.get_body(data)
+        
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+        # if not an http return 404 Npt FOund?
+        if('http' not in url and 'http'.upper() not in url and 'https' not in url and 'https'.upper() not in url):
+            code = 404
+            body = 'HTTP/1.1 404 Not Found'
+            return HTTPResponse(code, body)
+           
+        # parse url
+        self.parse(url)
+        if(args != None):
+            body = urlencode(args)
+            length = f"\nContent-Length: {str(len(body))}"
+        else:
+            length = f"\nContent-Length: {str(0)}"
+            body = ""
+        
+        #length = f"\nCOntent-Length: {len(body)}"
+        print("Length is: ", length)
+        request = f"POST {(self.path)} HTTP/1.1\r\nHost: {(self.host_name)}Connection: close\r\nContent-Type: application/x-www-form-urlencoded\r{(length)}\r\n\r\n{(body)}"
+        print("POST REQUEST IS: ", request)
+
+        # connect 
+        self.connect(self.host, self.port)
+        self.sendall(request)
+        data = self.recvall(self.socket)
+        self.close()
+
+        # get code and body then return response
+        code = self.get_code(data)
+        body = self.get_body(data)
+        
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
@@ -93,3 +165,12 @@ if __name__ == "__main__":
         print(client.command( sys.argv[2], sys.argv[1] ))
     else:
         print(client.command( sys.argv[1] ))
+
+
+
+# TO CITE:
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/POST
+# https://docs.plone.org/develop/plone/serving/http_request_and_response.html
+# https://docs.python.org/3/library/urllib.parse.html
+#
+#
